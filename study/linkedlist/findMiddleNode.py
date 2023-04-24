36e8ef19ad7ab247de1cbeabab36429d68311566
Rayco Haex <raycoh1299@gmail.com>
2023-04-24 21:46:35 +0200

    moved to new file

---
+++ study/linkedlist/findMiddleNode.py
@@ -0,0 +1,29 @@
+# exericse
+from LinkedList import *
+
+# linked_list = LinkedList(5)
+# linked_list.append(10)
+# linked_list.append(15)
+# linked_list.append(20)
+# linked_list.append(13)
+
+# linked_list.print_list()
+
+# print('---')
+
+# linked_list.set_value(1, 99)
+# linked_list.insert(2, 105)
+# linked_list.pop_first()
+
+# linked_list.print_list()
+
+# print('---')
+
+# linked_list.remove(1)
+
+# linked_list.print_list()
+
+# print('---')
+
+# linked_list.reverse()
+# linked_list.print_list()
