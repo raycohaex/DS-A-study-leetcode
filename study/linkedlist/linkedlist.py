94958e8b29caebac3d391b0bc1aa7c04430249f1
Rayco Haex <raycoh1299@gmail.com>
2023-04-24 21:30:23 +0200

    cleaned up code

--- study/linkedlist/linkedlist.py
+++ study/linkedlist/linkedlist.py
@@ -124,32 +124,3 @@ class LinkedList:
             temp.next = before
             before = temp
             temp = after
-
-
-
-linked_list = LinkedList(5)
-linked_list.append(10)
-linked_list.append(15)
-linked_list.append(20)
-linked_list.append(13)
-
-linked_list.print_list()
-
-print('---')
-
-linked_list.set_value(1, 99)
-linked_list.insert(2, 105)
-linked_list.pop_first()
-
-linked_list.print_list()
-
-print('---')
-
-linked_list.remove(1)
-
-linked_list.print_list()
-
-print('---')
-
-linked_list.reverse()
-linked_list.print_list()
