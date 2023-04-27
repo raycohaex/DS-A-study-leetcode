8e1a662d4f5bf32333a7c00e5db083c5a0aa9c11
Rayco Haex <raycoh1299@gmail.com>
2023-04-27 21:38:29 +0200

    added prepend

--- study/doublylinkedlist/DoublyLinkedList.py
+++ study/doublylinkedlist/DoublyLinkedList.py
@@ -43,6 +43,18 @@ class DoublyLinkedList:
             temp.prev = None
         self.length -= 1
         return temp
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.length += 1
+        return True
 
     
 
@@ -52,5 +64,7 @@ dll.append(2)
 dll.append(3)
 
 dll.pop()
+
+dll.prepend(0)
 
 dll.print_list()
