392a769885ad8d8830cd0dcca2ed95512df026d8
Rayco Haex <raycoh1299@gmail.com>
2023-04-27 21:36:15 +0200

    added dll pop

--- study/doublylinkedlist/DoublyLinkedList.py
+++ study/doublylinkedlist/DoublyLinkedList.py
@@ -30,9 +30,27 @@ class DoublyLinkedList:
         self.length += 1
         return True
 
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.tail
+        if self.length == 1:
+            self.head = None
+            self.tail = None 
+        else:       
+            self.tail = self.tail.prev
+            self.tail.next = None
+            temp.prev = None
+        self.length -= 1
+        return temp
+
+    
+
 dll = DoublyLinkedList(0)
 dll.append(1)
 dll.append(2)
 dll.append(3)
+
+dll.pop()
 
 dll.print_list()
