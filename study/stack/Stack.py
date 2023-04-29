1de534746e83852fd5b4951e64a556e352366210
Rayco Haex <raycoh1299@gmail.com>
2023-04-29 20:57:29 +0200

    added push to stack

--- study/stack/Stack.py
+++ study/stack/Stack.py
@@ -15,5 +15,15 @@ class Stack:
             print(temp.value)
             temp = temp.next
 
+    def push(self, value):
+        new_node = Node(value)
+        if self.height == 0:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self.height += 1
+
 stack = Stack(4)
+stack.push(5)
 stack.print_stack()
