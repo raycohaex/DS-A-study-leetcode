87e46f92d548e8c92ca48b08367d8e6b8f0bf790
Rayco Haex <raycoh1299@gmail.com>
2023-05-05 22:06:36 +0200

    insert bst

--- study/bst/bst.py
+++ study/bst/bst.py
@@ -4,8 +4,39 @@ class Node:
         self.next = None
         self.prev = None
         
-class binarysearchtree:
-    def __init__(self, value):
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
         new_node = Node(value)
-        self.root = new_node
-        self.length = 1
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while (True):
+            if new_node.value == temp.value:
+                return False
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else: 
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+tree = BinarySearchTree()
+tree.insert(10)
+tree.insert(5)
+tree.insert(13)
+tree.insert(11)
+tree.insert(2)
+
+print(tree.root.value)
+print(tree.root.left.value)
+print(tree.root.right.value)
+print(tree.root.left.left.value)
+print(tree.root.left.right.value)
